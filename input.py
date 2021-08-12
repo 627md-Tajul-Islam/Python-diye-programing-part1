32542ce0b783426b272203c38851944b84b4d1e4
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-12 16:56:26 -0700

    inputs added

---
+++ input.py
@@ -0,0 +1,41 @@
+# page no 24
+name = input("What is your name?")
+print("Welcome to Programing", name)
+
+# page no 25
+num1 = input("Type an integer:")
+num2 = input("Type another integer:")
+num1 = int(num1)
+num2 = int(num2)
+print("Total = ", num1+num2)
+# input function always returns a string
+
+num3 = input("Type an integer:")
+num4 = input("Type another integer:")
+num3 = int(num3)
+num4 = int(num4)
+print("Total = ", num3-num4)
+
+num5 = input("Type an integer:")
+num6 = input("Type another integer:")
+num5 = int(num5)
+num6 = int(num6)
+print("Total = ", num5*num6)
+
+num7 = input("Type an integer:")
+num8 = input("Type another integer:")
+num7 = int(num7)
+num8 = int(num8)
+print("Total = ", num7/num8)
+
+num9 = input("Type an integer:")
+num10 = input("Type another integer:")
+num9 = int(num9)
+num10 = int(num10)
+print("Total = ", num9//num10)
+
+num11 = input("Type an integer:")
+num12 = input("Type another integer:")
+num11 = int(num11)
+num12 = int(num12)
+print("Total = ", num11**num12)
