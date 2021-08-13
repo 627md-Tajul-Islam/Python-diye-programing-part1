2c7ec325ae226c205e6a113faf10019ff9302db0
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-12 18:43:27 -0700

    Comparison operator added

---
+++ comparison operators.py
@@ -0,0 +1,44 @@
+num1 = 2
+num2 = 3
+num3 = num1 == num2
+print(num3)
+#it will return false  beacuse the value are not same
+
+
+num4 = 3
+num5 = 3
+num6 = num4 == num5
+print(num6)
+#it will return true cause the value is same
+
+
+
+num7 = 2
+num8 = 3
+num9 = num7 > num8
+print(num9)
+#it will return false cause 2 is smaller than 3
+
+num10 = 2
+num11 = 3
+num12 = num10 < num11
+print(num12)
+#it will return true cause 3 is bigger than 2
+
+num13 = 2
+num14 = 3
+num15 = num13 != num14
+print(num15)
+#it will return true cause their value is not same
+
+num16 = 2
+num17 = 3
+num18 = num16 >= num17
+print(num18)
+#it will return false cause neither 2 is greater than 3 or their value is not same
+
+num19 = 2
+num20 = 3
+num21 = num19 <= num20
+print(num21)
+#it will return true cause 3 is greater than 2 and also their value are not same
