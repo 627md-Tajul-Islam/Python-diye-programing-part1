d1e815599551e1261aa4ad5ba2c4ab21e812cb1c
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-13 23:49:31 -0700

    Max number problem solving(Part 1)

---
+++ max number.py
@@ -0,0 +1,30 @@
+
+n1 = 30
+n2 = 40
+n3 = 10
+
+if n1 > n2 and n1 > n3:
+    print("Max Number: ", n1)
+
+elif n2 > n1 and n2 > n3:
+     print("Max Number: ", n2)
+
+else:
+     print("Max Number: ", n3)
+"""
+
+# another method
+n4 = 30
+n5 = 40
+n6 = 50
+if n4 > n5:
+   max = n4
+   print("Max", n4)
+else  :
+    max = n5
+    print("Max", n5)
+if n6 > max:
+   max = n6
+   print("Max", n6)
+   
+"""
