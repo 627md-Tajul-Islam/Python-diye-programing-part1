a9d7b07e27d4ab4e29f76dc2eb16a32028702fa6
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-13 17:00:15 -0700

    if else added

---
+++ saarc.py
@@ -0,0 +1,10 @@
+saarch = ["bangladesh","afhanistan","india","nepal",]
+
+country = input("enter name:")
+
+if country in saarch:
+    print(country, "is a member")
+else:
+    print("not a member")
+
+print("thanks for using")
