1f3df09db766ec801cb4f430a3015dc66ce2f633
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-15 03:18:29 -0700

    for loop 1st part

--- For loop.py
+++ For loop.py
@@ -1,2 +1,42 @@
-for i in range(100):
-    print("I love ALLAH")
+#for i in range(5):
+   # print("I love ALLAH")
+""" 
+import turtle
+turtle.shape("turtle")
+turtle.speed(1)
+
+for i in range(4):
+    turtle.forward(100)
+    turtle.left(90)
+turtle.exitonclick()
+"""
+""" 
+result = 0
+for i in range(5):
+    result = result + 1
+    print(result)
+
+
+
+result = 0
+num = 1
+for _ in range(50):
+    result = result + num
+print(result)
+
+
+result = 0
+for num in range(51):
+    result = result + num
+
+print(result)
+"""
+""" 
+result = 0
+for num in range(1,51):
+    result = result + num
+print(result)
+"""
+
+for i in range (1,20,5):
+    print(i)
