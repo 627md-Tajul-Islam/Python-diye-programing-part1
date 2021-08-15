2687fce3d76020a7df998b1f26d1da11514692c9
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-14 18:48:10 -0700

    Leap Year(Part 2 added)

--- Leap Year.py
+++ Leap Year.py
@@ -1,4 +1,6 @@
-year = input("Enter year")
+
+#Method 1
+year = input("Enter year :")
 year = int(year)
 if year % 4 == 0:
     if year % 100 == 0:
@@ -10,3 +12,22 @@ if year % 4 == 0:
         print("yes")
 else:
     print("No")
+
+print("Program Terminated")
+#Method 2
+year = input("Enter Year :")
+year = int(year)
+
+if year % 4 !=0:
+    print("No")
+else:
+    if year % 100 !=0:
+        print("yes")
+    else:
+        if year%400 !=0:
+            print("No")
+        else:
+            print("Yes")
+
+print("Program Terminated")
+
