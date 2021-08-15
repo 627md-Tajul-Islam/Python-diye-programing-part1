7be41f698d3acb42be9b1398caeb8c713f40b1c0
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-14 18:48:33 -0700

    Leap Year(Part 3 added)

--- Leap Year.py
+++ Leap Year.py
@@ -31,3 +31,16 @@ else:
 
 print("Program Terminated")
 
+#Method 3
+year = input("Enter Year :")
+year = int(year)
+
+if year% 400 == 0:
+    print("Yes")
+elif year%100 == 0:
+    print("No")
+elif year%4 == 0:
+    print("Yes")
+else:
+    print("No")
+
