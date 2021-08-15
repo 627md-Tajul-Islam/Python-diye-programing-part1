fab5e5cafa78f498b12e00e3fe112481021ba7be
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-14 20:55:58 -0700

    turtle triangle done

--- Turtle.py
+++ Turtle.py
@@ -1,5 +1,4 @@
 import turtle
-
 turtle.shape("circle")
 turtle.speed(1)
 
---
+++ polygon turtle.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.shape("triangle")
+turtle.speed(2)
+
+turtle.forward(300)
+turtle.left(130) # angle
+turtle.forward(300)
+turtle.left(115) # angle
+turtle.forward(242)
