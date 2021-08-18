1085d5a15aa0d8fc76e0843750bd9c546f787c62
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-17 21:49:42 -0700

    Loop on list

---
+++ Loop on List.py
@@ -0,0 +1,10 @@
+saarch = ["Bangladesh","Afghanistan","Bhutan","Nepal","India","Pakistan","Srilanka"]
+
+for country in saarch:
+    print(country, "Is a member or SAARCH")
+
+list = list(range(11))
+print(list)
+
+#li = list(range(2,21,2))
+#print(li)
