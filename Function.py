e271a3bb26c4c680ecd8989bbc33de920c8517a0
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-19 16:37:27 -0700

    function

---
+++ Function.py
@@ -0,0 +1,23 @@
+#def means definition
+def add(n1,n2):
+    return n1 + n2
+
+# output 1
+a = 10
+b = 15
+result = add(a,b)
+print(result)
+
+# output 2
+number1 = 10
+number2 = 10
+result = add(number1,number2)
+print(result)
+
+# output 3
+n1 = 5
+n2 = 10
+print(add(n1,n2))
+
+# output 4
+print(add(10,90))
---
+++ test.py
@@ -0,0 +1,5 @@
+name = "tajul"
+names = "tajullll"
+
+print("Our new student name is",name,names)
+print(name + names +"lives in jashore")
