7ce05b35ad03b1c6fcb7d3435b7575cfe094d7fb
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-22 23:32:37 -0700

    com

--- ListOnFunc.py
+++ ListOnFunc.py
@@ -8,10 +8,3 @@ def add_numbers(numbers):
 result = add_numbers([1,2,30,4,5,9])
 print(result)
 '''
-
-def test_fnc(li):
-    li[0] = 10
-
-list = [1,2,3,4]
-print("Before FUnction Call", list)
-test_fnc(list)
