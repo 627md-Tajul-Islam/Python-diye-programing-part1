93e8731d6df4849bd5e05cb76e758fa1fa3777cb
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-23 02:23:35 -0700

    lists

--- ListOnFunc.py
+++ ListOnFunc.py
@@ -26,3 +26,12 @@ print("Before Function Call", my_list)
 test(my_list)
 print("After Function Call", my_list)
 """
+
+list1 = [1,2,3,4]
+list2 = list1
+print(list1)
+print(list2)
+
+list2[0] = 100
+print(list2)
+print(list1)
