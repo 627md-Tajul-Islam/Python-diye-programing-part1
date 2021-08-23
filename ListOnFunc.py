c6da2b947435042936b4cccd4d00a0296adee8e0
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-08-23 00:28:26 -0700

    list

--- ListOnFunc.py
+++ ListOnFunc.py
@@ -8,3 +8,20 @@ def add_numbers(numbers):
 result = add_numbers([1,2,30,4,5,9])
 print(result)
 '''
+
+def add_numbers(numbers):
+    result = 0
+    for number in numbers:
+        result = result + 1
+    return result
+
+result = add_numbers([1,2,30,4,50])
+print(result)
+
+def test(li):
+    li[0] = 10
+
+my_list = [1,2,3,4,5]
+print("Before Function Call", my_list)
+test(my_list)
+print("After Function Call", my_list)
