c3ea6d71ec29fc0110443363e7b874bbb6097f5d
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-08 00:41:52 -0700

    String added

--- String Turtle.py
+++ String Turtle.py
@@ -4,4 +4,5 @@ name = turtle.textinput("name", "What is your name?")
 name = name.lower()
 if name.startswith("Md"):
     print("Assalamualaikum, May Allah Bless You")
-elif
+elif name.startswith("Sree"):
+    print("Get in hell")
