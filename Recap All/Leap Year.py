6ce31e065cc29169e8d0075930a8d33f10fb653c
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-20 22:06:01 -0700

    recap

--- Recap All/Leap Year.py
+++ Recap All/Leap Year.py
@@ -2,7 +2,8 @@ year = int(input("Enter a year: "))
 
 if year % 4 == 0:
     if year % 100 == 0:
-        print(year, "is a leap year")
+        if year % 400 == 0:
+            print(year, "is a leap year")
 else:
     print("THis is not a leap year")
 
