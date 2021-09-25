2e777e6f304864108c30ee59639fd716f24bf36e
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-24 19:13:08 -0700

    recap

--- Recap All/AllinONE.py
+++ Recap All/AllinONE.py
@@ -2,3 +2,6 @@ kill = False
 while not kill:
     num1 = int(input("Enter 1st Number: "))
     num2 = int(input("Enter 2nd Number: "))
+
+    while True:
+        operation = input("Please enter add/sub/multiply/division (quit to exit):  ")
