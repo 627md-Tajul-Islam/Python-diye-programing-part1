4beb297dd17d54eed818bf697691ecf1494e460a
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-24 18:50:01 -0700

    recap

--- Recap All/square.py
+++ Recap All/square.py
@@ -1,5 +1,5 @@
 while True:
-    num = int(input("Enter a number (but dont use 0): "))
+    num = int(input("Enter a number (Press 0 to close): "))
     if num == 0:
         break
     print("Square of", num , "is", num * num)
