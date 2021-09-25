7b26d00a34097884dbcd3d0b56968939d0ad28ab
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-09-24 19:28:57 -0700

    recap

--- Recap All/AllinONE.py
+++ Recap All/AllinONE.py
@@ -8,11 +8,13 @@ while not kill:
         if action == "quit":
             kill = True
             break
-        if action not in ["add","sub","multiple","division"]:
+        if action not in ["add","sub","multiply","division"]:
             print("Action Unknown !")
         if action == "add":
             print("Sum of 2 numbers is:", num1 + num2)
             break
         if action == "sub":
             print("Subtraction of 2 numbers is ", num1 - num2)
+        if action == "multiply":
+            print("Multiplication of 2 numbers is ", num1 * num2)
 
