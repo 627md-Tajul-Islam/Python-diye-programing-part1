ef4277d8be586acaf80aca05d66c8cf6bfddc056
Md. Tajul Islam <tajul.azazil@gmail.com>
2021-10-08 20:04:28 -0700

    recap

--- Recap All/set actions.py
+++ Recap All/set actions.py
@@ -1,6 +1,10 @@
 A = {1,2,3,4,5}
 B = {2,4,6,8,10}
 
-# createing a new set using intersection, taking common
+# creating a new set using intersection, taking common items
 intersection = A & B
 print(intersection)
+
+# Union means taking
+union = A | B
+print(union)
